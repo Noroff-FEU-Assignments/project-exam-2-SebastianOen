63ac2a83fbd3b16a06e98c0d30ab5ad9e0f5a808
Sebastian Øen <83211414+SebastianOen@users.noreply.github.com>
2024-02-03 12:32:57 +0100

    Readme.md updated

--- project-exam/src/components/Posts/AllPosts.jsx
+++ project-exam/src/components/Posts/AllPosts.jsx
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
-import styles from "./allposts.module.css";
+import styles from "./Allposts.module.css";
 import Comments from "../Comments/Comments";
 import Spinner from "react-bootstrap/Spinner";
 import DeleteButton from "./DeleteButton";
